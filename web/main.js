2ee481bad74ee0e7f202d3cf1e7d36a32b8c40af
James Ngandu <jamesmngandu@gmail.com>
2023-10-09 10:09:58 +0300

    added

---
+++ web/main.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector(".clicker");
+
+btn.addEventListener("click", ()=>{
+
+    prompt("Confirm your not a robot");
+    alert("The system is under mantainance")
+    alert("Please wait ...")
+})
