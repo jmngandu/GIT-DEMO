9839ed0d367dd8c8c0718499ef285ad644173855
James Ngandu <jamesmngandu@gmail.com>
2023-11-01 14:03:58 +0300

    feat:get started

--- web/main.js
+++ web/main.js
@@ -1,8 +1,6 @@
-let btn = document.querySelector(".clicker");
+let btn = document.querySelector('.clicker');
 
-btn.addEventListener("click", ()=>{
-
-    prompt("Confirm your not a robot");
-    alert("The system is under mantainance")
-    alert("Please wait ...")
-})
+btn.addEventListener('click', () => {
+	prompt('Confirm your not a robot');
+	alert('Please wait ...');
+});
